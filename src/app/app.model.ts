78bbc6b9cddc708600dc239e96c3d8dca9088f6f
Helias <stefanoborzi32@gmail.com>
2022-08-08 21:10:23 +0200

    chore: fix import path

--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -1,4 +1,4 @@
-import { systemIDs } from './home/home.model';
+import { systemIDs } from './components/home/home.model';
 
 export const enum RealSynthetic {
   BOTH,
@@ -12,22 +12,8 @@ export const enum DataType {
   LOUD_NORM_DATA,
 }
 
-export const SPEAKERS_A01_A06 = [
-  'LA_0069',
-  'LA_0070',
-  'LA_0071',
-  'LA_0072',
-  'LA_0073',
-  'LA_0074',
-  'LA_0075',
-];
-export const SPEAKERS_A07_A19 = [
-  'LA_0012',
-  'LA_0013',
-  'LA_0047',
-  'LA_0023',
-  'LA_0038',
-];
+export const SPEAKERS_A01_A06 = ['LA_0069', 'LA_0070', 'LA_0071', 'LA_0072', 'LA_0073', 'LA_0074', 'LA_0075'];
+export const SPEAKERS_A07_A19 = ['LA_0012', 'LA_0013', 'LA_0047', 'LA_0023', 'LA_0038'];
 
 export const SYSTEM_IDS: systemIDs[] = ['A01_A06', 'A07_A19'];
 export const FEATURES = [
