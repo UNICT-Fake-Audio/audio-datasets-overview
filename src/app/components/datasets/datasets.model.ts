9b2811a9a7803a9f793465d8cd898be74f7602de
Helias <stefanoborzi32@gmail.com>
2023-05-29 23:09:58 +0200

    rename only_silence -> silence

--- src/app/components/datasets/datasets.model.ts
+++ src/app/components/datasets/datasets.model.ts
@@ -12,20 +12,20 @@ export interface QueryParameters {
 
 export const DATASETS = [
   'ASVSPOOF_2019_LA',
-  'ASVSPOOF_2019_LA_ONLY_SILENCE',
+  'ASVSPOOF_2019_LA_SILENCE',
   // 'ASVSPOOF_2019_LA_MP3_LOW',
   // 'ASVSPOOF_2019_LA_MP3_MEDIUM',
   // 'ASVSPOOF_2019_LA_MP3_HIGH',
   'ASVSPOOF_2021',
-  'ASVSPOOF_2021_ONLY_SILENCE',
+  'ASVSPOOF_2021_SILENCE',
   'FAKE_OR_REAL',
-  'FAKE_OR_REAL_ONLY_SILENCE',
+  'FAKE_OR_REAL_SILENCE',
   'IN_THE_WILD',
-  'IN_THE_WILD_ONLY_SILENCE',
+  'IN_THE_WILD_SILENCE',
   'FAD',
   'SOS',
 ] as const;
-export type Dataset = typeof DATASETS[number];
+export type Dataset = (typeof DATASETS)[number];
 
 export const SYNTHETIC_LABELS = ['spoof', 'fake'];
 
